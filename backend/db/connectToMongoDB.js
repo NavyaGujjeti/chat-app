6c2fca46d68665d3208e84e38ae235e37449c309
NavyaGujjeti <navyagujjet123@gmail.com>
2025-04-08 20:50:12 +0530

    url changed

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect("mongodb+srv://navya:navya123@cluster0.lbptk3w.mongodb.net/chat-app?retryWrites=true&w=majority&appName=Cluster0");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
